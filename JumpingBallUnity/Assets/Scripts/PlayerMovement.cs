225852ed212e97d49513fb3952934d256536042d
çağdaş bilgin <cagdasbilgin1@gmail.com>
2021-04-06 15:09:43 +0300

    feat: add player movement

---
+++ JumpingBallUnity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool jump = false;
+    Rigidbody rb;
+    Transform cameraHolder;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpForce;
+    Vector3 vec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cameraHolder = Camera.main.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector3.forward * playerSpeed * Time.fixedDeltaTime);
+        if (jump)
+        {
+            rb.AddForce(Vector3.up * jumpForce*1000 * Time.fixedDeltaTime);
+            jump = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        vec.x = cameraHolder.transform.position.x;
+        vec.y = cameraHolder.transform.position.y;
+        vec.z = transform.position.z;
+
+        cameraHolder.transform.position = vec;
+
+    }
+}
